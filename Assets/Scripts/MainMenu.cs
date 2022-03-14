d4b4ee8a3ff881f27c007c4d6e5005fa72971604
RobyRAX <robyalexander571@gmail.com>
2022-03-14 20:59:00 +0700

    main menu

---
+++ Assets/Scripts/MainMenu.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{
+    public GameObject CreditPanel;
+
+    public void PlayButton()
+    {
+        SceneManager.LoadScene("SampleScene");
+    }
+
+    public void CreditButton()
+    {
+        CreditPanel.SetActive(true);
+    }
+
+    public void CloseCredit()
+    {
+        CreditPanel.SetActive(false);
+    }
+}
